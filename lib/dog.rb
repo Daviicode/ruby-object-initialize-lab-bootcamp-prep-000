7f627c8b6becddac16ee98adee71e747d447402d
David zhang <d.ydzdavid@yahoo.com>
2019-03-04 15:10:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,18 +3,4 @@ class Dog
     @name = name 
     @breed = breed
   end
-  
-  def name 
-    @name
-  end
-  
-  def breed
-    @breed
-  end
-  
-  def breed=(breed)
-    @breed = "Mutt"
-  end
-end
-
-  
+ 
